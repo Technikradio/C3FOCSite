d608c110aa08895c001f289d9788d33fcebd369f
Doralitze <technikradio@icloud.com>
2017-09-15 16:31:50 +0200

    This should be enougth to render a nav bar. Keep in mind that this piece of code is untested and will be checked once I've got a working infrastructure again.

---
+++ c3shop/frontpage/uitools/navbar.py
@@ -0,0 +1,25 @@
+from ..models import Settings
+from django.http import HttpRequest
+import json
+
+NAV_BAR_SETTINGS_KEY: str = "frontpage.ui.navbar.content"
+
+
+def process_link(position: int, item):
+    return '<a href="' + item.href + '">' + item.text + '</a>'
+
+
+def render_nav_bar(request: HttpRequest):
+    try:
+        feature_line: str = Settings.objects.find(SName=NAV_BAR_SETTINGS_KEY).property
+        parts = json.loads(feature_line)
+        a = '<div class="nav">'
+        pos = -1
+        for item in parts:
+            pos += 1
+            if str(item.type) == "link":
+                a += process_link(pos, item)
+        a += '</div>'
+        return a
+    except:
+        return "<h3>An error was thrown resulting in the incapability to display the nav bar</h3>"
