d189c078d01dbfd1742b1df847d60df41c13fb3b
Doralitze <technikradio@icloud.com>
2017-09-25 17:16:14 +0200

    Added basic listing of existing posts There's still some work to do.

--- c3shop/frontpage/management/post_page.py
+++ c3shop/frontpage/management/post_page.py
@@ -1,7 +1,36 @@
 from django.http import HttpRequest
+from ..models import Post
+
+
+def generate_edit_link(p: Post):
+    return "/admin/posts/edit?post_id=" + str(p.pk)
 
 
 def render_post_list(request: HttpRequest):
-    a = "<h3>Posts:</h3>"
+    # TODO add method to select how many posts to display
+    # TODO create icon for post writing
+    # TODO add way to determine maximum page
+    # TODO make layout more fancy
+    page = 1
+    items_per_page = 50
+    if request.GET.get('page'):
+        page = int(request.GET["page"])
+    if request.GET.get('objects'):
+        items_per_page = int(request.GET["objects"])
+    start_range = 1 + page * items_per_page
+    end_range = (page + 1) * items_per_page
+    a = '<h3>Posts:</h3><a href="/admin/posts/edit">Add a new Post</a>' \
+        '<table><tr><th> Post ID </th><th>Post title</th><th> visibility level</th>' \
+        '<th> Author </th></tr>'
+    objects = Post.objects.filter(pk__rage=(start_range, end_range))
+    for post in objects:
+        a += '<a href="' + generate_edit_link(post) + '"><tr><td>' + str(post.pk) + "</td><td>" + post.title + \
+             "</td><td>" + str(post.visibleLevel) + "</td><td>" + str(post.createdByUser.authuser.username) + \
+             "</td></tr></a>"
+    a += '</table>'
+    if page > 1:
+        a += '<a href="' + request.path + '?page=' + str(page - 1) + '&objects=' + str(objects) + '">Previous page </a>'
+    a += '<a href="' + request.path + '?page=' + str(page + 1) + '&objects=' + str(objects) + '">Next page </a>'
+    a += '<center>displaying page ' + str(page) + '.</center>'
 
     return a
