aa1cefc2d978c626bef31b95a040a056b98cfef1
Doralitze <technikradio@icloud.com>
2017-11-09 13:48:22 +0100

    Implemented adding article to reservation cookie

--- c3shop/frontpage/management/edit_reservation.py
+++ c3shop/frontpage/management/edit_reservation.py
@@ -1,13 +1,34 @@
-from django.http import HttpRequest
-from django.shortcuts import redirect
+from django.http import HttpRequest, HttpResponseRedirect
+import json
+
+RESERVATION_CONSTRUCTION_COOKIE_KEY: str = "c3shop.frontpage.reservation.cookiekey"
 
 
 def add_article_action(request: HttpRequest, default_foreward_url: str):
     forward_url = default_foreward_url
     # TODO fix
-    return redirect(forward_url)
+    current_reservation = json.loads(request.COOKIES.get(RESERVATION_CONSTRUCTION_COOKIE_KEY))
+    id: int = int(request.POST.get("article_id"))
+    quantity : int = int(request.POST["quantity"])
+    current_reservation.get("articles").append({"id": id, "quantity": quantity})
+    response = HttpResponseRedirect(forward_url)
+    response.set_cookie(RESERVATION_CONSTRUCTION_COOKIE_KEY, json.dumps(current_reservation, indent=4))
+    return response
 
 
 def create_reservation_action(request: HttpRequest):
+    """
+    This function is used to add a reservation to the database from the
+    cookie stored inside the client. This function automatically crafts
+    the required HttpResponse.
+    """
     # TODO fix
+    pass
+
+
+def manipulate_reservation_action(request: HttpRequest):
+    """
+    This function is used to alter the reservation beeing build inside
+    a cookie. This function automatically crafts the required response.
+    """
     pass
