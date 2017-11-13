27137a8cc950dd2fa6ac16a4bc9b1b1906fdbd37
Doralitze <technikradio@icloud.com>
2017-11-13 14:12:49 +0100

    Wrote save action TODO: Rename file to reservation_actions.py TODO: implement frontend

--- c3shop/frontpage/management/edit_reservation.py
+++ c3shop/frontpage/management/edit_reservation.py
@@ -1,16 +1,21 @@
 from django.http import HttpRequest, HttpResponseRedirect
+from django.shortcuts import redirect
+from ..models import GroupReservation, ArticleRequested, Article
+from .magic import get_current_user
 import json
 
 RESERVATION_CONSTRUCTION_COOKIE_KEY: str = "c3shop.frontpage.reservation.cookiekey"
 
 
 def add_article_action(request: HttpRequest, default_foreward_url: str):
-    forward_url = default_foreward_url
-    # TODO fix
+    forward_url: str = default_foreward_url
+    if request.GET.get("redirect"):
+        forward_url = request.GET["redirect"]
     current_reservation = json.loads(request.COOKIES.get(RESERVATION_CONSTRUCTION_COOKIE_KEY))
-    id: int = int(request.POST.get("article_id"))
+    aid: int = int(request.POST.get("article_id"))
     quantity : int = int(request.POST["quantity"])
-    current_reservation.get("articles").append({"id": id, "quantity": quantity})
+    notes: str = request.POST["notes"]
+    current_reservation.get("articles").append({"id": aid, "quantity": quantity, "notes": notes})
     response = HttpResponseRedirect(forward_url)
     response.set_cookie(RESERVATION_CONSTRUCTION_COOKIE_KEY, json.dumps(current_reservation, indent=4))
     return response
@@ -22,8 +27,29 @@ def create_reservation_action(request: HttpRequest):
     cookie stored inside the client. This function automatically crafts
     the required HttpResponse.
     """
-    # TODO fix
-    pass
+    forward_url = "/admin/reservations"
+    if request.GET.get("redirect"):
+        forward_url = request.GET["redirect"]
+    current_reservation = json.loads(request.COOKIES.get(RESERVATION_CONSTRUCTION_COOKIE_KEY))
+    r: GroupReservation = GroupReservation()
+    r.createdByUser = get_current_user()
+    r.open = True
+    r.notes = current_reservation.get("notes")
+    r.pickupDate = current_reservation.get("pickup_date")  # TODO parse to date
+    r.ready = False
+    r.save()
+    for arts: current_reservation.get("articles"):
+        aid = int(arts.get("id"))
+        q = int(arts.get("quantity"))
+        notes = arts.get("notes")
+        a: Article = Article.objects.get(pk=aid)
+        req: ArticleRequested = ArticleRequested()
+        req.RID = r
+        req.AID = a
+        req.amount = q
+        req.notes = notes
+        req.save()
+    return redirect(forward_url)
 
 
 def manipulate_reservation_action(request: HttpRequest):
