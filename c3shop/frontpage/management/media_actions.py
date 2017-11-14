025f29ea2a29096fbea2785053684e2c175819f9
Doralitze <technikradio@icloud.com>
2017-11-14 21:46:51 +0100

    Fixed upload bug
    Make sure to edit the default upload path and host it using Nginx

--- c3shop/frontpage/management/media_actions.py
+++ c3shop/frontpage/management/media_actions.py
@@ -5,6 +5,10 @@ from ..models import Profile, Media, MediaUpload
 from .magic import compile_markdown, get_current_user
 
 import logging
+import os
+
+
+PATH_TO_UPLOAD_FOLDER_ON_DISK: str = "./"
 
 
 def action_change_user_avatar(request: HttpRequest):
@@ -22,8 +26,11 @@ def action_change_user_avatar(request: HttpRequest):
 
 def handle_file(u: Profile, headline: str, category: str, text: str, file):
     m: Media = Media()
-    high_res_file_name = 'uploads/' + str(date.today().year) + '/HIGHRES_' + file.name.replace(" ", "_")
-    low_res_file_name = 'uploads/' + str(date.today().year) + '/LOWRES_' + file.name.replace(" ", "_")
+    upload_base_path: str = 'uploads/' + str(date.today().year)
+    high_res_file_name = upload_base_path + '/HIGHRES_' + file.name.replace(" ", "_")
+    low_res_file_name = upload_base_path + '/LOWRES_' + file.name.replace(" ", "_")
+    if not os.path.exists(PATH_TO_UPLOAD_FOLDER_ON_DISK + upload_base_path):
+        os.makedirs(PATH_TO_UPLOAD_FOLDER_ON_DISK + upload_base_path)
     with open(high_res_file_name, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
@@ -31,15 +38,15 @@ def handle_file(u: Profile, headline: str, category: str, text: str, file):
     m.text = text
     m.cachedText = compile_markdown(text)
     m.category = category
-    m.highResFile = high_res_file_name
-    m.lowResFile = low_res_file_name
+    m.highResFile = "/" + high_res_file_name
+    m.lowResFile = "/" + low_res_file_name
     m.headline = headline
     m.save()
     mu: MediaUpload = MediaUpload()
     mu.UID = u
     mu.MID = m
     mu.save()
-    logging.info("Uploaded file '" + file.name + "' and cropped it. The resulting PK is " + m.pk)
+    logging.info("Uploaded file '" + str(file.name) + "' and cropped it. The resulting PK is " + str(m.pk))
 
 
 def action_add_single_media(request: HttpRequest):
