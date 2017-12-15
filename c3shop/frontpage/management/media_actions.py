9f78be4aa6e46c9c3e7806f23246276f91071450
Doralitze <technikradio@icloud.com>
2017-12-15 21:38:38 +0100

    Trying to fix resize filter

--- c3shop/frontpage/management/media_actions.py
+++ c3shop/frontpage/management/media_actions.py
@@ -7,6 +7,7 @@ from .magic import compile_markdown, get_current_user
 import logging
 import os
 import math
+import PIL
 from PIL import Image
 
 
@@ -47,7 +48,7 @@ def handle_file(u: Profile, headline: str, category: str, text: str, file):
     height /= diameter
     width *= IMAGE_SCALE
     height *= IMAGE_SCALE
-    cropped = original.resize(width, height, Image.LANCZOS)
+    cropped = original.resize(width, height, PIL.Image.LANCZOS)
     cropped.save(low_res_file_name)
     m.text = text
     m.cachedText = compile_markdown(text)
