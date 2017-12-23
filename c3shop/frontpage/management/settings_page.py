56cca13917ea736ca0422ca68ace18ca09edc151
Doralitze <technikradio@icloud.com>
2017-12-23 12:34:15 +0100

    Fixed a minor typo

--- c3shop/frontpage/management/settings_page.py
+++ c3shop/frontpage/management/settings_page.py
@@ -8,7 +8,7 @@ def render_settings_page(request: HttpRequest):
     a = '''<div class="w3-row w3-padding-64 w3-twothird w3-container">
     <h3 class="w3-text-teal">Header content</h3>At the moment the header contains
     the following content:<p><code>%S%</code></p><br />It was last modified by: %U%
-    <br /><a href="/admin/settings/edit-header"><img src="/staticfiles/frontpage/edit"
+    <br /><a href="/admin/settings/edit-header"><img src="/staticfiles/frontpage/edit.png"
     alt="edit" class="button-img"/></a>
     </div>'''.replace('%S%', m.property).replace('%U%', render_user_link(m.changedByUser))
     m = Settings.objects.get(SName='frontpage.ui.footer.content')
