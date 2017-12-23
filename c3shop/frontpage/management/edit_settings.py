c20df71c560bab2f634252aa8aa9122cbfb0adc6
Doralitze <technikradio@icloud.com>
2017-12-23 13:18:13 +0100

    Fixed the bug causing the wrong setting to be edited

--- c3shop/frontpage/management/edit_settings.py
+++ c3shop/frontpage/management/edit_settings.py
@@ -3,10 +3,10 @@ from ..models import Settings
 from .form import PlainText, TextArea, SubmitButton, Form, TextField
 
 
-def render_text_based_panel(request: HttpRequest, setting: str, a1: str, a2: str):
+def render_text_based_panel(request: HttpRequest, setting: str, a1: str, a2: str, action: str):
     s: Settings = Settings.objects.get(SName=setting)
     f: Form = Form()
-    f.action_url = "/admin/actions/set-header"
+    f.action_url = action
     f.add_content(PlainText('<h3 class="w3-text-teal">' + a1 + '</h3>'))
     f.add_content(TextArea(name="property", label_text=a2,
         text=s.property, placeholder="Please define the content of the header using JSON"))
@@ -21,10 +21,10 @@ def render_text_based_panel(request: HttpRequest, setting: str, a1: str, a2: str
 
 def render_header_edit_panel(request: HttpRequest):
     return render_text_based_panel(request, 'frontpage.ui.navbar.content', 'Edit header content:',
-            'Edit the content of the nav bar:')
+            'Edit the content of the nav bar:', "/admin/actions/set-header")
 
 
 def render_footer_edit_panel(request: HttpRequest):
     return render_text_based_panel(request, 'frontpage.ui.footer.content', 'Edit footer content:',
-            'Edit the content of the footer:')
+            'Edit the content of the footer:', "/admin/actions/set-footer")
 
