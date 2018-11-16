c0f3460a28d2773df62e8a8616ff7fccbf144c22
Doralitze <technikradio@icloud.com>
2018-11-16 23:00:15 +0100

    Got working QR codes

--- c3shop/frontpage/management/export.py
+++ c3shop/frontpage/management/export.py
@@ -5,8 +5,17 @@ from django.http import HttpResponse, HttpRequest, HttpResponseForbidden
 from ..models import GroupReservation
 from .magic import timestamp
 import logging
+import qrcode
 
 logger = logging.getLogger(__name__)
+
+
+def generateQRLink(link: str):
+    qr = qrcode.QRCode(version=None, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 30, border = 4)
+    qr.add_data(link)
+    qr.make(fit=True)
+    return qr.make_image()
+
 
 def exportOrderToPDF(request: HttpRequest, res):
     # Setup the canvas
@@ -24,19 +33,29 @@ def exportOrderToPDF(request: HttpRequest, res):
     p : canvas = canvas.Canvas(buffer, pagesize=A4, pageCompression=0)
     # Render PDF
     w, h = A4
+    p.setTitle("C3FOC - Reservations")
+    p.setAuthor("The robots in slavery by " + request.user.username)
+    p.setSubject("This document, originally created at " + timestamp() + ", contains the requested reservations")
     for reservation in reservations:
         r: GroupReservation = reservation # Just for the sake of having a shourtcut
-        p.setFont("Helvetica", 14)
-        #p.rotate(180)
-        p.drawString(10, h - 100, "Reservation by: " + str(r.createdByUser.displayName))
-        p.line(0, 15, w, 15)
-        # TODO generate finished link QR code
-        text = p.beginText(15, 30)
-        text.textLines(r.notes)
-        p.drawText(text)
-        p.showPage() # End page here (one per request)
+        if r.open and not r.ready:
+            p.setFont("Helvetica", 9)
+            p.rotate(90)
+            p.drawString(34, -25, "Reservation #" + str(r.id))
+            p.rotate(270)
+            p.setFont("Helvetica", 14)
+            # Render header
+            p.drawString(25, h - 35, "Reservation by: " + str(r.createdByUser.displayName))
+            p.line(25, h - 45, w - 25, h - 45)
+            # render qr code
+            i = generateQRLink(request.build_absolute_uri('/') + "admin/confirm?back_url=/admin/reservations&forward_url=/admin/reservations/finish&payload=" + str(r.id))
+            p.drawInlineImage(i, w - 150, h - 175, 125, 125)
+            # TODO generate finished link QR code
+            text = p.beginText(30, h - 75)
+            text.textLines(r.notes)
+            p.drawText(text)
+            p.showPage() # End page here (one per request)
     #Close the canvas
-    p.showPage()
     p.save()
     pdf = buffer.getvalue()
     buffer.close()
