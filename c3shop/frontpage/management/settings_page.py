0f65fc1a6b215aed3ba3e9f74cd815f8e5946de9
Doralitze <technikradio@icloud.com>
2018-12-21 11:43:39 +0100

    add: Database export button in settings

--- c3shop/frontpage/management/settings_page.py
+++ c3shop/frontpage/management/settings_page.py
@@ -6,13 +6,14 @@ from ..uitools.body import render_user_link
 def render_settings_page(request: HttpRequest):
     m: Settings = Settings.objects.get(SName='frontpage.ui.navbar.content')
     a = '''<div class="admin-popup w3-row w3-padding-64 w3-twothird w3-container">
+    <a href="/admin/export?method=datadump" class="button">Export Database</a>
     <h3 class="w3-text-teal">Header content</h3>At the moment the header contains
     the following content:<p><code>%S%</code></p><br />It was last modified by: %U%
     <br /><a href="/admin/settings/edit-header"><img src="/staticfiles/frontpage/edit.png"
     alt="edit" class="button-img"/></a>
-    </div>'''.replace('%S%', m.property).replace('%U%', render_user_link(m.changedByUser))
+    '''.replace('%S%', m.property).replace('%U%', render_user_link(m.changedByUser))
     m = Settings.objects.get(SName='frontpage.ui.footer.content')
-    a += '''<div class="w3-row admin-popup w3-padding-64 w3-twothird w3-container">
+    a += '''
     <h3 class="w3-text-teal">Footer content</h3>At the moment the header contains
     the following content:<p><code>%S%</code></p><br />It was last modified by: %U%
     <br /><a href="/admin/settings/edit-footer"><img src="/staticfiles/frontpage/edit.png"
